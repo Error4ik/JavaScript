012cdd5f543f6faa646549e6f76ed994708f547d
Error4ik <fearerror@yandex.ru>
2019-12-14 23:27:53 +0300

    Getting items on a page.

---
+++ training/basics/get_items/main.js
@@ -0,0 +1,21 @@
+'use strict'; //TODO Строгий режим стандарта ES6
+
+let box = document.getElementById('box');
+console.log(box);
+
+let button = document.getElementsByTagName('button');
+console.log(button);
+console.log(button[2]);
+
+let wrapperBtn = document.querySelectorAll('.wrapper button');
+console.log(wrapperBtn);
+console.log(wrapperBtn[4]);
+
+let circle = document.getElementsByClassName('circle');
+console.log(circle);
+console.log(circle[2]);
+
+let heart = document.querySelectorAll('.heart');
+console.log(heart);
+console.log(heart[1]);
+
