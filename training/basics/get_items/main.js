f1e2052bf39dcad86eeefde94a0e1dc155a76ec4
Error4ik <fearerror@yandex.ru>
2019-12-15 23:50:55 +0300

    Use getting items

--- training/basics/get_items/main.js
+++ training/basics/get_items/main.js
@@ -1,21 +1,44 @@
 'use strict'; //TODO Строгий режим стандарта ES6
 
+let body = document.body;
 let box = document.getElementById('box');
-console.log(box);
-
-let button = document.getElementsByTagName('button');
-console.log(button);
-console.log(button[2]);
-
+let button = document.getElementsByClassName('btn');
 let wrapperBtn = document.querySelectorAll('.wrapper button');
-console.log(wrapperBtn);
-console.log(wrapperBtn[4]);
-
 let circle = document.getElementsByClassName('circle');
-console.log(circle);
-console.log(circle[2]);
-
 let heart = document.querySelectorAll('.heart');
-console.log(heart);
-console.log(heart[1]);
+let wrapper = document.querySelector('.wrapper');
 
+box.style.background = 'blue';
+button[1].style.borderRadius = '100%';
+circle[0].style.background = 'red';
+circle[1].style.background = 'yellow';
+circle[2].style.background = 'green';
+
+for (let i = 0; i < heart.length; i++) {
+    heart[i].style.background = 'yellow';
+}
+
+heart.forEach(function (item) {
+    item.style.background = 'green';
+});
+
+let div = document.createElement('div');
+// let textNode = document.createTextNode('Hello all!');
+
+div.classList.add('newDiv');
+div.classList.add('textNode');
+// div.appendChild(textNode);
+
+// //TODO Добавить элемент в дом.
+// body.appendChild(div);
+wrapper.appendChild(div);
+div.textContent = 'Garry!';
+
+//TODO добавить элемент в дом, перед другим элементом.
+// body.insertBefore(div, circle[1]);
+
+//TODO Удалить элемент.
+body.removeChild(heart[2]);
+wrapper.removeChild(wrapperBtn[2]);
+
+body.replaceChild(box, circle[2]);
