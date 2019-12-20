850421386f4f90acf333e28e27a5b491fbba1fa2
Error4ik <fearerror@yandex.ru>
2019-12-20 20:28:03 +0300

    Forms js complete.

--- training/basics/forms/main.js
+++ training/basics/forms/main.js
@@ -1,1 +1,24 @@
 'use strict'; //TODO Строгий режим стандарта ES6
+
+let aboutForm = document.getElementById("about-form");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let about = document.getElementById("about");
+
+aboutForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let array = aboutForm.querySelectorAll('input');
+
+    let test = {
+
+    };
+
+    for (let i = 0; i < array.length; i++) {
+        test[array[i].name] = array[i].value;
+    }
+    console.log(test);
+
+    let formData = new FormData(aboutForm);
+    console.log(formData);
+});
