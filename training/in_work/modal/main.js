ef278530c7e2cf6ac6d11af2359d18e6a14d7a48
Error4ik <fearerror@yandex.ru>
2019-12-27 00:03:43 +0300

    Modal fix.

--- training/in_work/modal/main.js
+++ training/in_work/modal/main.js
@@ -8,9 +8,13 @@ window.addEventListener('DOMContentLoaded', function () {
 
     moreBtn.addEventListener('click', function () {
         modal.style.display = 'block';
+        //запретить прокрутку страницы при открытом модальном окне.
+        document.body.style.overflow = 'hidden';
     });
 
     close.addEventListener('click', function () {
         modal.style.display = 'none';
+        //разрешить прокрутку после закрытия модального окна.
+        document.body.style.overflow = '';
     })
 });
