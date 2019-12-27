f7c95a9d994184472cf52f130a0b2bd292d09163
Error4ik <fearerror@yandex.ru>
2019-12-27 23:31:20 +0300

    Let and const.

---
+++ training/in_work/let_const/main.js
@@ -0,0 +1,40 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    'use strict'; //TODO Строгий режим стандарта ES6
+
+    // function makeArray() {
+    //     var items = [];
+    //
+    //     //TODO var i создалась одна на весь цикл.
+    //     for (var i = 0; i < 10; i++) {
+    //         var item = function () {
+    //             console.log(i);
+    //         };
+    //         items.push(item);
+    //     }
+    //     return items;
+    // }
+    //
+    // function makeArray2() {
+    //     var items = [];
+    //
+    //     //TODO с let.
+    //     for (let i = 0; i < 10; i++) {
+    //         var item = function () {
+    //             console.log(i);
+    //         };
+    //         items.push(item);
+    //     }
+    //     return items;
+    // }
+    //
+    // var arr = makeArray();
+    // var arr2 = makeArray2();
+    //
+    // arr[1]();
+    // arr[3]();
+    // arr[7]();
+    // arr2[1]();
+    // arr2[3]();
+    // arr2[7]();
+});
