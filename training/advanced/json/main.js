2469f42ddfac0c251f493e12d7c0f3da8d51ffc1
Error <fearerror@yandex.ru>
2020-01-05 13:13:17 +0300

    Json.

--- training/advanced/json/main.js
+++ training/advanced/json/main.js
@@ -1,4 +1,20 @@
 window.addEventListener('DOMContentLoaded', function () {
     "use strict";
 
+    let options =  {
+        width: 800,
+        height: 600,
+        background: 'blue',
+        font: {
+            size: 20 + 'px',
+            color: 'white'
+        }
+    };
+    console.log("Default - ", options);
+
+    let json = JSON.stringify(options);
+    console.log("Json - ", json);
+
+    let parse = JSON.parse(json);
+    console.log("Parse - ", parse);
 });
